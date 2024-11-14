d4076aeb7d77176d45b08d8964e3cda402bd0431
itsmekatrine <itsmekatrine1@gmail.com>
2024-11-14 20:49:52 +0300

    Убрала приватные статические поля, оставила только вызов конструктора

--- src/com/yandex/app/service/Managers.java
+++ src/com/yandex/app/service/Managers.java
@@ -1,14 +1,12 @@
 package com.yandex.app.service;
 
 public class Managers {
-    private static final TaskManager manager = new InMemoryTaskManager();
-    private static final HistoryManager history = new InMemoryHistoryManager();
 
     public static TaskManager getDefault() {
-        return manager;
+        return new InMemoryTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
-        return history;
+        return new InMemoryHistoryManager();
     }
 }
