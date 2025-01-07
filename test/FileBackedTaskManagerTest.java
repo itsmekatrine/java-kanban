4e4135a7952180303acdb5fcc4cca316abb941ee
itsmekatrine <itsmekatrine1@gmail.com>
2025-01-07 16:00:05 +0300

    Исправления для checkstyle

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -3,7 +3,6 @@ import com.yandex.app.model.Epic;
 import com.yandex.app.model.Subtask;
 import com.yandex.app.model.Task;
 import com.yandex.app.service.FileBackedTaskManager;
-import com.yandex.app.service.HistoryManager;
 import com.yandex.app.service.InMemoryHistoryManager;
 import com.yandex.app.service.Managers;
 import org.junit.jupiter.api.*;
--- test/InMemoryTaskManagerTest.java
+++ test/InMemoryTaskManagerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
