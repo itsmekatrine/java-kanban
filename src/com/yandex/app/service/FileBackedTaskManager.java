e98030c685e8bcf4042a18356925eacfcc0181d8
itsmekatrine <itsmekatrine1@gmail.com>
2025-01-08 16:19:33 +0300

    Исправления checkstyle

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -11,7 +11,6 @@ import java.nio.file.Files;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     private final File file;
